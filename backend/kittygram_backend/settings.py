bbb9f6ca7462a98696d60a282b276033d8de0c07
Vadim Trakhimets <154251081+Tantal25@users.noreply.github.com>
2024-04-17 10:09:43 +0800

    Первая итерация ревью с правками

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,16 +2,18 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
 
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key()),
 
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG_SETTING', 'False'))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kitgrmplus.ru']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
