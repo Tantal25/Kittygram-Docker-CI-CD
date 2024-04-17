5b410a8452034326f4c4d01cf7dde9f885b7909b
Vadim Trakhimets <154251081+Tantal25@users.noreply.github.com>
2024-04-17 11:28:10 +0800

    Проверка ошибки settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,11 +10,13 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-# os.getenv('SECRET_KEY', get_random_secret_key())
+#os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG_SETTING', 'False')
+DEBUG = True
+#os.getenv('DEBUG_SETTING', 'False')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kitgrmplus.ru']
+#os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
